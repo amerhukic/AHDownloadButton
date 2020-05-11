b2705a0feb41426b908c98471769eaabfb0e4eb8
Jan Rajtr <rajtrj@cngroup.dk>
2020-05-11 13:57:48 +0200

    - updated package file

--- Package.swift
+++ Package.swift
@@ -1,11 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "AHDownloadButton",
+    platforms: [
+        .iOS(.v8),
+    ],
     products: [
-        .executable(name: "AHDownloadButton", targets: ["AHDownloadButton"]),
+        .library(name: "AHDownloadButton", targets: ["AHDownloadButton"])
+        
     ],
     targets: [
         .target(
