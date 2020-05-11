e0ab9c2ce54fd72fe45826b92ef70a4ecbfbf2c1
Jan Rajtr <rajtrj@cngroup.dk>
2020-05-11 12:17:27 +0200

    package.swift added

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "AHDownloadButton",
+    products: [
+        .executable(name: "AHDownloadButton", targets: ["AHDownloadButton"]),
+    ],
+    targets: [
+        .target(
+            name: "AHDownloadButton",
+            dependencies: ["AHDownloadButton"]),
+    ]
+)
