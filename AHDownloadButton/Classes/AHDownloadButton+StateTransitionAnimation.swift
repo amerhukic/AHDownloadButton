ad0bd916c537493c434f57dc19f5a2702a84949d
Amer Hukic <hukicamer@gmail.com>
2020-12-12 22:36:50 +0100

    Fix issue where dispatch group leave was not called if the app was sent to the background

--- AHDownloadButton/Classes/AHDownloadButton+StateTransitionAnimation.swift
+++ AHDownloadButton/Classes/AHDownloadButton+StateTransitionAnimation.swift
@@ -12,10 +12,9 @@ extension AHDownloadButton {
     
     func animateTransition(from oldState: State, to newState: State) {
         
-        let completion: (Bool) -> Void = { completed in
-            guard completed else { return }
-            self.resetStateViews(except: newState)
+        let completion: (Bool) -> Void = { _ in
             self.animationDispatchGroup.leave()
+            self.resetStateViews(except: newState)
         }
                 
         switch (oldState, newState) {
@@ -49,7 +48,6 @@ extension AHDownloadButton {
             self.layoutIfNeeded()
         }, completion: { completed in
             completion(completed)
-            guard completed else { return }
             self.pendingCircleView.alpha = 1
             self.pendingCircleView.startSpinning()
         })
@@ -62,7 +60,6 @@ extension AHDownloadButton {
             self.layoutIfNeeded()
         }, completion: { completed in
             completion(completed)
-            guard completed else { return }
             self.downloadingButton.alpha = 1
         })
     }
